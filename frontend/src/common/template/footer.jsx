fb9f5c09e0742ee9ebed75d23bf609aa99c237d8
Amanda <amanda@M01DEV04.local>
2018-08-21 10:09:33 -0300

    aqui coloco uma mensagem, tipo: comitando primeiro post

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2018
+            <a href='https://github.com/amandaacilene' target='_blank'> Amanda</a>
+        </strong>
+    
+    </footer>
+)
